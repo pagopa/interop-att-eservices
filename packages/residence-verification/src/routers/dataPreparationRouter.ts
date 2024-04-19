6f0073075a6be61087e94d2098ee7f5377aced0f
LorenzoRita <lorenzo.rita@accenture.com>
2024-04-19 09:59:53 +0200

    added renamed files

---
+++ packages/residence-verification/src/routers/dataPreparationRouter.ts
@@ -0,0 +1,124 @@
+//import { zodiosRouter } from "@zodios/express";
+import { ZodiosRouter } from "@zodios/express";
+import { ZodiosEndpointDefinitions } from "@zodios/core";
+
+import { ErrorHandling, UserModel } from "pdnd-models";
+import { ExpressContext, ZodiosContext, logger } from "pdnd-common";
+import DataPreparationService from "../services/dataPreparationService.js";
+import {
+  userModelToApiDataPreparationResponseCf,
+  userModelToApiDataPreparationTemplateResponse,
+} from "../model/domain/apiConverter.js";
+import { api } from "../model/generated/api.js";
+import { createEserviceDataPreparation } from "../exceptions/errorMappers.js";
+import { makeApiProblem, userModelNotFound } from "../exceptions/errors.js";
+import { DataPreparationTemplateResponse } from "../model/domain/models.js";
+
+const dataPreparationRouter = (ctx: ZodiosContext): ZodiosRouter<ZodiosEndpointDefinitions, ExpressContext> => {
+  const dataPreparationRouter = ctx.router(api.api);
+
+dataPreparationRouter.post(
+  "/residence-verification/data-preparation",
+  async (req, res) => {
+    try {
+      const data = await DataPreparationService.saveList(req.body);
+      const result = userModelToApiDataPreparationResponseCf(
+        data,
+        req.body.soggetto?.codiceFiscale
+      );
+      if (!result) {
+        throw userModelNotFound(`Data with fiscal code '${req.body.soggetto?.codiceFiscale}' not found`);
+      }
+      return res.status(200).json(result).end();
+    } catch (error) {
+      const errorRes = makeApiProblem(error, createEserviceDataPreparation);
+      return res.status(errorRes.status).json(errorRes).end();
+    }
+  }
+);
+
+dataPreparationRouter.get(
+  "/residence-verification/data-preparation",
+  async (req, res) => {
+    try {
+      if (!req) {
+        throw ErrorHandling.invalidApiRequest();
+      }
+      const result: DataPreparationTemplateResponse[] = [];
+      const data = await DataPreparationService.getAll();
+
+      if (data != null) {
+        data.forEach((user: UserModel) => {
+          result.push(userModelToApiDataPreparationTemplateResponse(user));
+        });
+      }
+      return res.status(200).json(result).end();
+    } catch (error) {
+      const errorRes = makeApiProblem(error, createEserviceDataPreparation);
+      return res.status(errorRes.status).json(errorRes).end();
+    }
+  }
+);
+
+dataPreparationRouter.get(
+  "/residence-verification/data-preparation/:uuid",
+  async (req, res) => {
+    try {
+      if (!req) {
+        return res.status(500);
+      }
+      let result: DataPreparationTemplateResponse | null = null;
+      const data = await DataPreparationService.getByUUID(req.params.uuid);
+
+      if (data) {
+        result = userModelToApiDataPreparationTemplateResponse(data);
+        return res.status(200).json(result).end();
+      }
+      return res.status(200).end();
+    } catch (error) {
+      const errorRes = makeApiProblem(error, createEserviceDataPreparation);
+      return res.status(errorRes.status).json(errorRes).end();
+    }
+  }
+);
+
+dataPreparationRouter.delete(
+  "/residence-verification/data-preparation",
+  async (req, res) => {
+    try {
+      if (!req) {
+        throw ErrorHandling.invalidApiRequest();
+      }
+      const data = await DataPreparationService.deleteAllByKey();
+      logger.info("data " + data);
+      if (data !== 0) {
+        throw ErrorHandling.genericError(`Not all data could be deleted. Remaining: ${data}`);
+      }
+      return res.status(200).end();
+    } catch (error) {
+      const errorRes = makeApiProblem(error, createEserviceDataPreparation);
+      return res.status(errorRes.status).json(errorRes).end();
+    }
+  }
+);
+
+dataPreparationRouter.delete(
+  "/residence-verification/data-preparation/:uuid",
+  async (req, res) => {
+    try {
+      if (!req) {
+        return res.status(500);
+      }
+      await DataPreparationService.deleteByUUID(req.params.uuid);
+      return res.status(200).end();
+    } catch (error) {
+      const errorRes = makeApiProblem(error, createEserviceDataPreparation);
+      return res.status(errorRes.status).json(errorRes).end();
+    }
+  }
+  
+);
+return dataPreparationRouter;
+
+}
+export default dataPreparationRouter;
---
+++ packages/residence-verification/src/services/dataPreparationService.ts
@@ -0,0 +1,123 @@
+import { logger } from "pdnd-common";
+import { UserModel } from "pdnd-models";
+import { v4 as uuidv4 } from "uuid";
+import { getContext } from "pdnd-common";
+import { DataPreparationTemplate } from "../model/domain/models.js";
+import dataPreparationRepository from "../repository/dataPreparationRepository.js";
+import { apiDataPreparationTemplateToUserModel } from "../model/domain/apiConverter.js";
+import generateHash from "../utilities/hashUtilities.js";
+import {
+  appendUniqueUserModelsToArray,
+  findUserModelByUUID,
+  deleteUserModelByUUID,
+} from "../utilities/userUtilities.js";
+
+class DataPreparationService {
+  appContext = getContext();
+
+  public async saveList(
+    genericRequest: DataPreparationTemplate
+  ): Promise<UserModel[] | null> {
+    try {
+      logger.info(`[START] saveList`);
+      const userData: UserModel[] = [];
+      userData.push(
+        apiDataPreparationTemplateToUserModel(genericRequest, uuidv4())
+      );
+      const hash = generateHash([this.appContext.authData.purposeId]);
+      const persistedUserData = await dataPreparationRepository.findAllByKey(
+        hash
+      );
+      if (persistedUserData == null || persistedUserData.length == 0) {
+        await dataPreparationRepository.saveList(userData, hash);
+      } else {
+        const allUser = appendUniqueUserModelsToArray(
+          persistedUserData,
+          userData
+        );
+        await dataPreparationRepository.saveList(allUser, hash);
+      }
+      const response = await dataPreparationRepository.findAllByKey(hash);
+      logger.info(`[END] saveList`);
+      return response;
+    } catch (error) {
+      logger.error(
+        `saveList - Errore durante il salvataggio della lista.`,
+        error
+      );
+      throw error;
+    }
+  }
+
+  public async getAll(): Promise<UserModel[] | null> {
+    try {
+      logger.info(`[START] getAll`);
+      const hash = generateHash([this.appContext.authData.purposeId]);
+      const response = await dataPreparationRepository.findAllByKey(hash);
+      logger.info(`[END] getAll`);
+      return response;
+    } catch (error) {
+      logger.error(
+        `getAll: Errore durante il recupero della lista.`,
+        error
+      );
+      throw error;
+    }
+  }
+
+  public async getByUUID(uuid: string): Promise<UserModel | null> {
+    try {
+      logger.info(`[START] getByUUID`);
+      const hash = generateHash([this.appContext.authData.purposeId]);
+      const result = await dataPreparationRepository.findAllByUuid(hash, uuid);
+      const response = findUserModelByUUID(result, uuid);
+      logger.info(`[START] getByUUID`);
+      return response;
+    } catch (error) {
+      logger.error(
+        `UserService: Errore durante il salvataggio della lista. `,
+        error
+      );
+      throw error;
+    }
+  }
+
+  public async deleteAllByKey(): Promise<number | null> {
+    try {
+      logger.info(`[START] deleteAllByKey`);
+      const hash = generateHash([this.appContext.authData.purposeId]);
+      const response = await dataPreparationRepository.deleteAllByKey(hash);
+      logger.info(`[END] deleteAllByKey`);
+      return response
+    } catch (error) {
+      logger.error(
+        `UserService: Errore durante la cancellazione della lista. `,
+        error
+      );
+      throw error;
+    }
+  }
+
+  public async deleteByUUID(uuid: string): Promise<UserModel[] | null> {
+    try {
+      logger.info(`[START] deleteByUUID`);
+      const hash = generateHash([this.appContext.authData.purposeId]);
+      const allSaved = await dataPreparationRepository.findAllByKey(hash);
+      const user = deleteUserModelByUUID(allSaved, uuid);
+      this.deleteAllByKey();
+      if (user) {
+        dataPreparationRepository.saveList(user, hash);
+      }
+      logger.info(`[END] deleteByUUID`);
+      return user;
+    } catch (error) {
+      logger.error(
+        `deleteByUUID - Errore durante l'aggiornamento della lista.`,
+        error
+      );
+      throw error;
+    }
+  }
+}
+
+export default new DataPreparationService();
