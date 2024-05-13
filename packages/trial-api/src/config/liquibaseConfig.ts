2c7faf95c0a96a2ed99ff72c65bc939fef0efedd
marta.boccanelli <marta.boccanelli@accenture.com>
2024-05-13 09:08:24 +0200

    fix liquibase config

--- packages/trial-api/src/config/liquibaseConfig.ts
+++ packages/trial-api/src/config/liquibaseConfig.ts
@@ -1,24 +1,27 @@
-import * as fs from "fs";
 import { Liquibase, LiquibaseConfig } from "liquibase";
 import { logger } from "pdnd-common";
+import dotenv from "dotenv";
 
-const configPath = "../../packages/trial-api/liquibase.json"; // Percorso del file di configurazione Liquibase
+// Carica le variabili di ambiente da .env
+dotenv.config();
 
-// Leggi il file di configurazione come stringa
-const configString = fs.readFileSync(configPath, "utf-8");
-
-// Converti la stringa JSON in un oggetto JavaScript
-const configData = JSON.parse(configString);
-
+// Verifica se le variabili d'ambiente necessarie sono definite
+if (!process.env.LIQUIBASE_DATABASE_URL || !process.env.LIQUIBASE_DATABASE_USERNAME || !process.env.LIQUIBASE_DATABASE_PASSWORD || !process.env.LIQUIBASE_CHANGELOG_FILE) {
+  throw new Error("Una o più variabili d'ambiente necessarie non sono definite.");
+}
 // Crea un oggetto di configurazione Liquibase
 const liquibaseConfig: LiquibaseConfig = {
-  ...configData, // Assicurati che il formato di liquibase.json sia corretto
+  url: process.env.LIQUIBASE_DATABASE_URL, 
+  username: process.env.LIQUIBASE_DATABASE_USERNAME, 
+  password: process.env.LIQUIBASE_DATABASE_PASSWORD, 
+  changeLogFile: process.env.LIQUIBASE_CHANGELOG_FILE, 
 };
 
 // Crea un'istanza di Liquibase con l'oggetto di configurazione
 const liquibase = new Liquibase(liquibaseConfig);
 
 export async function runLiquibase(): Promise<void> {
+
   try {
     await liquibase.update({}); // Passa un oggetto di configurazione vuoto
     logger.info("Modifiche al database applicate con successo.");
