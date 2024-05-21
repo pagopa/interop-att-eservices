de9b08818afb0410b6a51a625ee051f6a4bc438c
marta.boccanelli <marta.boccanelli@accenture.com>
2024-05-21 14:31:22 +0200

    feat(ADA-37): serial number cert

--- packages/fiscalcode-verification/src/utilities/certificateUtility.ts
+++ packages/fiscalcode-verification/src/utilities/certificateUtility.ts
@@ -1,5 +1,5 @@
-import crypto from "crypto";
 import { logger } from "pdnd-common";
+import { X509 } from 'jsrsasign';
 
 export function getCertificateFingerprintFromBuffer(buffer: Buffer): string {
   const cert: string = buffer.toString('utf8');
@@ -7,12 +7,18 @@ export function getCertificateFingerprintFromBuffer(buffer: Buffer): string {
   // Converte il certificato pulito in formato URL-encoded
   const encodedCert = encodeURIComponent(cert);
   logger.info ("encodedCert: " + encodedCert);
-  return getCertificateFingerprintFromString(encodedCert)
+  logger.info ("serial number: " + getSerialNumber(cert));
+
+  return getSerialNumber(cert);
+  //return getCertificateFingerprintFromString(encodedCert)
 }
 
 export function getCertificateFingerprintFromString(certString: string): string {
+  logger.info ("encodedCert: " + certString);
+  logger.info ("serial number: " + getSerialNumber(certString));
   // Rimuovi i delimitatori e unisci le linee del certificato
-  const certBody = certString
+  return getSerialNumber(certString);
+  /*const certBody = certString
     .replace(/-----BEGIN CERTIFICATE-----/, '')
     .replace(/-----END CERTIFICATE-----/, '')
     .replace(/\s+/g, '') // Rimuove gli spazi bianchi e le nuove linee
@@ -24,5 +30,14 @@ export function getCertificateFingerprintFromString(certString: string): string 
   // Calcola l'hash SHA-256 del Buffer
   const hash = crypto.createHash('sha256');
   hash.update(certBuffer);
-  return hash.digest('hex');
+  return hash.digest('hex');*/
 }
+
+
+// Funzione per estrarre il numero di serie di un certificato
+export function getSerialNumber(cert: string): string {
+  const x509 = new X509();
+  x509.readCertPEM(cert);
+  return x509.getSerialNumberHex();
+}
+
