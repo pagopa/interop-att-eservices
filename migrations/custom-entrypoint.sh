30bbcc8c0b08219544d842e9b8618bc3d80ea9af
Carlucci  Francesco <francesco.carlucci@accenture.com>
2024-06-17 12:19:11 +0200

    feat(ADA-45): init container for data migration

--- migrations/custom-entrypoint.sh
+++ migrations/custom-entrypoint.sh
@@ -4,4 +4,7 @@
 find /flyway/sql/ -type f -name "*.sql" -exec sed -i "s/{{USER_PASSWORD}}/$DATABASE_USERNAME/g" {} \;
 find /flyway/sql/ -type f -name "*.sql" -exec sed -i "s/{{USER_NAME}}/$DATABASE_PASSWORD/g" {} \;
 
+find /flyway/sql/ -type f -name "*.sql" -exec cat {} \;
+
+
 /flyway/flyway "$@"
