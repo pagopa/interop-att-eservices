5541915b5f87072e3c655a596f869dc22e0734fe
Manuel Aru <manuelaru@MacBook-Pro-di-Manuel.local>
2025-03-25 10:01:45 +0100

    fix service file coordinateService

--- packages/residence-verification/src/services/coordinateService.ts
+++ packages/residence-verification/src/services/coordinateService.ts
@@ -1,32 +1,31 @@
 import NodeGeocoder from "node-geocoder";
 import { CoordinatesModel } from "pdnd-models";
 
-
 const geocoder = NodeGeocoder({
   provider: "openstreetmap",
 });
 
- class coordinatesService {
-   public async getCoordinates(
-     address: string,
-   ): Promise<CoordinatesModel | undefined> {
-     try {
-       const res = await geocoder.geocode(address);
-       if (res.length > 0) {
-         const coordinates: CoordinatesModel = {
-           latitude: res[0].latitude,
-           longitude: res[0].longitude,
-         };
-         return coordinates;
-       } else {
-         console.log("Nessun risultato trovato.");
-         return undefined;
-       }
-     } catch (error) {
-       console.error("Errore durante la geocodifica:", error);
-       return undefined;
-     }
-   }
- }
+class coordinatesService {
+  public async getCoordinates(
+    address: string
+  ): Promise<CoordinatesModel | undefined> {
+    try {
+      const res = await geocoder.geocode(address);
+      if (res.length > 0) {
+        const coordinates: CoordinatesModel = {
+          latitude: String(res[0].latitude),
+          longitude: String(res[0].longitude),
+        };
+        return coordinates;
+      } else {
+        console.log("Nessun risultato trovato.");
+        return undefined;
+      }
+    } catch (error) {
+      console.error("Errore durante la geocodifica:", error);
+      return undefined;
+    }
+  }
+}
 
- export default new coordinatesService();
+export default new coordinatesService();
