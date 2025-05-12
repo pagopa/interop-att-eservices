770d09531e69f9ee82a536b68cccbda33e6ddd2b
Giorgio Ceccherini <giorgio.ceccherini@mobilesoft.it>
2025-05-12 17:01:50 +0200

    Fix on connection pool

--- packages/trial-api/src/db/client.ts
+++ packages/trial-api/src/db/client.ts
@@ -5,7 +5,11 @@ const { Pool } = pkg;
 import * as schema from './schema/index.js';
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    host: process.env.DATABASE_HOST ?? 'localhost',
+    port: Number(process.env.DATABASE_PORT ?? '5432'),
+    user: process.env.DATABASE_USERNAME,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_NAME,
 });
 
 export const db = drizzle(pool, { schema });
