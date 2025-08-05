07bd02b2da200035e2e13f867a6ff017e75ac5a6
Benito Cassa <benito.cassa@external.mobilesoft.it>
2025-08-05 15:28:03 +0200

    Update infra-start.sh and update .env files.

--- scripts/infra-start.sh
+++ scripts/infra-start.sh
@@ -4,3 +4,37 @@ docker compose -f ../docker/docker-compose.yml up -d
 
 pnpm turbo install --filter pdnd-commons
 pnpm turbo run drizzle:migrate --filter pdnd-commons
+pnpm turbo run build --filter pdnd-commons
+
+kms_output=$(aws --endpoint-url=http://localhost:4566 kms create-key --key-usage SIGN_VERIFY --customer-master-key-spec RSA_2048)
+key_id=$(echo "$kms_output" | jq -r '.KeyMetadata.KeyId')
+
+packages=(
+  "digital-address-verification"
+  "faily-status"
+  "fiscalcode-verification"
+  "keychain-mock"
+  "piva-verification"
+  "residence-submission"
+  "residence-verification"
+  "residence-verification-direct"
+  "trial-service-api"
+)
+
+base_dir="../packages"
+
+for package in "${packages[@]}"; do
+  env_file="$base_dir/$package/.env"
+  if [ -f "$env_file" ]; then
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      sed -i '' '/^KMS_KEYID = /d' "$env_file"
+    else
+      sed -i '/^KMS_KEYID = /d' "$env_file"
+    fi
+  fi
+  echo "KMS_KEYID = $key_id" >> "$env_file"
+done
+
+echo "KMS_KEYID updated in all .env files" &&
+
+cd ../packages/trial-service-api && pnpm run start
