10fbb3ef43d0ad345e2c966508ef2ce61d4e3ab9
Giorgio Ceccherini <giorgio.ceccherini@mobilesoft.it>
2025-08-29 11:47:58 +0200

    Managed KMS client initialization for localstack use + new Environment variables for family status

--- packages/family-status/src/utilities/keychainSignatureUtility.ts
+++ packages/family-status/src/utilities/keychainSignatureUtility.ts
@@ -1,14 +1,29 @@
 import { logger } from "pdnd-common";
 import { KMSClient, SignCommand } from "@aws-sdk/client-kms";
+import { keychainSignerConfig } from "../config/keychainSignerConfig.js";
+
+const config = keychainSignerConfig();
 
 export class keychainSignatureUtility {
   private kmsClient: KMSClient;
   private keyId: string;
 
   constructor(keyId: string) {
-    this.kmsClient = new KMSClient({});
+    const isLocal = config.localKeychainConfig === "true";
+    if (isLocal) {
+      this.kmsClient = new KMSClient({
+        region: config.kmsRegion,
+        endpoint: config.kmsKeychainEndpoint,
+        credentials: {
+          accessKeyId: config.kmsAccessKeyId,
+          secretAccessKey: config.kmsAccessKeySecret,
+        },
+      });
+    } else {
+      this.kmsClient = new KMSClient({});
+    }
     this.keyId = keyId;
-    logger.info(`Client KMS inizializzato per la chiave: ${keyId}`);
+    logger.info(`Client KMS initialized with key: ${keyId}`);
   }
 
   /**
@@ -20,26 +35,24 @@ export class keychainSignatureUtility {
   public async signData(data: string): Promise<string> {
     try {
       const dataBuffer = Buffer.from(data, "utf-8");
-
       const signCommand = new SignCommand({
         KeyId: this.keyId,
         Message: dataBuffer,
         SigningAlgorithm: "RSASSA_PKCS1_V1_5_SHA_256",
       });
-
       const response = await this.kmsClient.send(signCommand);
       const signature = response.Signature
         ? Buffer.from(Uint8Array.from(response.Signature)).toString("base64")
         : null;
 
       if (!signature) {
-        throw new Error("La firma non è stata generata correttamente");
+        throw new Error("Signature generation failure");
       }
 
-      logger.info("Firma generata con successo");
+      logger.info("Signature successfully generated");
       return signature;
     } catch (error) {
-      logger.error("Errore durante la generazione della firma:", error);
+      logger.error("Error during signature generation:", error);
       throw error;
     }
   }
