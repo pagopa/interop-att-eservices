8016709bd1febf2106693a70e5c03850a3777d08
Benito Cassa <benito.cassa@external.mobilesoft.it>
2025-08-29 12:26:10 +0200

    Update script infra-start.sh.

--- scripts/infra-start.sh
+++ scripts/infra-start.sh
@@ -29,23 +29,34 @@ packages=(
   "trial-service-api"
 )
 
+update_env_variable() {
+  local env_file="$1"
+  local var_name="$2"
+  local value="$3"
+  local create_var="$4"
+
+  if grep -q "^$var_name = " "$env_file"; then
+    echo "Updating $var_name in $env_file"
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      sed -i '' "s/^$var_name = .*/$var_name = $value/" "$env_file"
+    else
+      sed -i "s/^$var_name = .*/$var_name = $value/" "$env_file"
+    fi
+  elif [ "$create_var" == "true" ]; then
+    echo "Adding $var_name to $env_file"
+    if [ -s "$env_file" ]; then
+      echo "" >> "$env_file"
+    fi
+    echo "$var_name = $value" >> "$env_file"
+  fi
+}
+
 for package in "${packages[@]}"; do
   env_file="packages/$package/.env"
+
   if [ -f "$env_file" ]; then
-    if grep -q '^KMS_KEYID = ' "$env_file"; then
-      echo "Updating KMS_KEYID in $env_file"
-      if [[ "$OSTYPE" == "darwin"* ]]; then
-        sed -i '' "s/^KMS_KEYID = .*/KMS_KEYID = $key_id/" "$env_file"
-      else
-        sed -i "s/^KMS_KEYID = .*/KMS_KEYID = $key_id/" "$env_file"
-      fi
-    else
-      echo "Adding KMS_KEYID to $env_file"
-      if [ -s "$env_file" ]; then
-        echo "" >> "$env_file"
-      fi
-      echo "KMS_KEYID = $key_id" >> "$env_file"
-    fi
+    update_env_variable "$env_file" "KMS_KEYCHAIN_PUBLICKEY_KID" "$key_id" "false"
+    update_env_variable "$env_file" "KMS_KEYID" "$key_id" "true"
   fi
 done
 
