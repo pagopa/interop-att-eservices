f0120f5d2b986613237272f1831139be1fbc35c8
Benito Cassa <benito.cassa@external.mobilesoft.it>
2025-11-27 15:42:57 +0100

    refactor(errors): remove userModelNotFound function and clean up residenceVerificationRouter.

--- packages/residence-verification/src/exceptions/errors.ts
+++ packages/residence-verification/src/exceptions/errors.ts
@@ -36,14 +36,6 @@ export function eServiceNotFound(eserviceId: string): ApiError<ErrorCodes> {
   });
 }
 
-export function userModelNotFound(details?: string): ApiError<ErrorCodes> {
-  return new ApiError({
-    detail: details != null ? details : "Data not found",
-    code: "userModelNotFound",
-    title: "Not found",
-  });
-}
-
 export function requestParamNotValid(details: string): ApiError<ErrorCodes> {
   return new ApiError({
     detail: details,
