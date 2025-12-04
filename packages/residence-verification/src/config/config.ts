a1ad37059b58e5600c47e491100aea9719ef78d7
Manuel Aru <manuelaru@MacBook-Pro-di-Manuel.local>
2025-12-04 12:19:31 +0100

    configuration integration of env files in the service residence verification package

---
+++ packages/residence-verification/src/config/config.ts
@@ -0,0 +1,45 @@
+import { z } from "zod";
+import {
+  LoggerConfig,
+  DatabaseConfig,
+  ShConfig,
+  M2mConfig,
+  SignerConfig,
+} from "pdnd-common";
+
+const ResidenceSpecificConfig = z
+  .object({
+    SKIP_JWT_VERIFICATION: z
+      .enum(["true", "false"])
+      .transform((v) => v === "true")
+      .default("false"),
+    WELL_KNOWN_URLS: z.string().optional(),
+
+    SKIP_INTEROPERABILITY_VERIFICATION: z
+      .enum(["true", "false"])
+      .transform((v) => v === "true")
+      .default("false"),
+    SKIP_AGID_PAYLOAD_VERIFICATION: z
+      .enum(["true", "false"])
+      .transform((v) => v === "true")
+      .default("false"),
+  })
+  .transform((env) => ({
+    skipJwtVerification: env.SKIP_JWT_VERIFICATION,
+    wellKnownUrls: env.WELL_KNOWN_URLS,
+    skipInteroperabilityVerification: env.SKIP_INTEROPERABILITY_VERIFICATION,
+    skipAgidPayloadVerification: env.SKIP_AGID_PAYLOAD_VERIFICATION,
+  }));
+
+export const ResidenceVerificationConfig = LoggerConfig.and(DatabaseConfig)
+  .and(ShConfig)
+  .and(M2mConfig)
+  .and(SignerConfig)
+  .and(ResidenceSpecificConfig);
+
+export type ResidenceVerificationConfig = z.infer<
+  typeof ResidenceVerificationConfig
+>;
+
+export const residenceVerificationConfig: ResidenceVerificationConfig =
+  ResidenceVerificationConfig.parse(process.env);
