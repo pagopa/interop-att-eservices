170d907fe07b5b81612b9b8dd170a667db6e5b76
Benito Cassa <benito.cassa@external.mobilesoft.it>
2025-12-05 16:54:47 +0100

    Make the HTTPS key and certificate paths optional in the SslConfig.

--- packages/commons/src/config/SslConfig.ts
+++ packages/commons/src/config/SslConfig.ts
@@ -2,12 +2,12 @@ import { z } from "zod";
 
 export const SslConfig = z
   .object({
-    HTTPS_KEY_PATH: z.string(),
-    HTTPS_CERT_PATH: z.string(),
+    HTTPS_KEY_PATH: z.string().optional(),
+    HTTPS_CERT_PATH: z.string().optional(),
   })
   .transform((c) => ({
-    httpsKeyPath: c.HTTPS_KEY_PATH,
-    httpsCertPath: c.HTTPS_CERT_PATH,
+    httpsKeyPath: c.HTTPS_KEY_PATH ?? null,
+    httpsCertPath: c.HTTPS_CERT_PATH ?? null,
   }));
 export type SslConfig = z.infer<typeof SslConfig>;
 
