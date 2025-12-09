ad1693074257a3cdd143dd34e6c23f2ae8db4868
Benito Cassa <benito.cassa@external.mobilesoft.it>
2025-12-09 10:35:54 +0100

    Refactor fiscalcodeVerificationConfig to remove unused configurations and streamline environment variable parsing.

--- packages/fiscalcode-verification/src/config/config.ts
+++ packages/fiscalcode-verification/src/config/config.ts
@@ -1,18 +1,8 @@
 import { z } from "zod";
-import {
-  LoggerConfig,
-  DatabaseConfig,
-  HTTPServerConfig,
-  SignerConfig,
-  JWTConfig,
-  SslConfig,
-} from "pdnd-common";
+import { DatabaseConfig, HTTPServerConfig, SslConfig } from "pdnd-common";
 
-export const FiscalcodeVerificationConfig = LoggerConfig.and(DatabaseConfig)
-  .and(HTTPServerConfig)
-  .and(SignerConfig)
-  .and(JWTConfig)
-  .and(SslConfig);
+export const FiscalcodeVerificationConfig =
+  DatabaseConfig.and(HTTPServerConfig).and(SslConfig);
 
 export type FiscalcodeVerificationConfig = z.infer<
   typeof FiscalcodeVerificationConfig
--- packages/fiscalcode-verification/src/index.ts
+++ packages/fiscalcode-verification/src/index.ts
@@ -2,9 +2,9 @@ import fs from "fs";
 import https from "https";
 import { logger, testDbConnection } from "pdnd-common";
 import app from "./app.js";
-import { fiscalcodeVerificationConfig } from "./config/config.js";
+import { FiscalcodeVerificationConfig } from "./config/config.js";
 
-const config = fiscalcodeVerificationConfig;
+const config = FiscalcodeVerificationConfig.parse(process.env);
 const port = config.port;
 const portHttps = Number(port) + 443;
 
