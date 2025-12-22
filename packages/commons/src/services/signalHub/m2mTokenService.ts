6508c1e38badd8e666afa2c5bd353eed9d2d7541
Lorenzo Giorgi <lorenzo.giorgi@pagopa.it>
2025-12-22 15:15:16 +0100

    Complete the refactoring to use a standard method to generate the M2M token in charge to authorize SH calls

--- packages/commons/src/services/signalHub/m2mTokenService.ts
+++ packages/commons/src/services/signalHub/m2mTokenService.ts
@@ -1,22 +1,11 @@
-import {
-  exec_pdnd_client_assertion_m2m,
-  get_pdnd_token_m2m,
-  logger,
-  M2mConfig,
-} from "../../index.js";
+import { get_pdnd_token_m2m, logger, M2mConfig } from "../../index.js";
 
 export async function getPDNDTokenM2M(
   config: M2mConfig
 ): Promise<string | undefined> {
   logger.info("Generating a new PDND M2M token...");
   try {
-    const client_assertion = exec_pdnd_client_assertion_m2m(config);
-    const token = await get_pdnd_token_m2m(await client_assertion, config);
-
-    if (!token) {
-      throw new Error("get_pdnd_token did not re  turn a token.");
-    }
-
+    const token = await get_pdnd_token_m2m(config);
     logger.info("New PDND M2M token generated successfully.");
     return token;
   } catch (error) {
